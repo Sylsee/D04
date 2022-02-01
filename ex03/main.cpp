6dd5bc6c8fc50f8d01b61a29c4740b4a6b6df449
Sylsee <sylvio.poliart@gmail.com>
2022-02-01 20:16:47 +0100

    clean

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: spoliart <spoliart@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 23:01:42 by spoliart          #+#    #+#             */
-/*   Updated: 2022/02/01 19:25:33 by spoliart         ###   ########.fr       */
+/*   Updated: 2022/02/01 20:13:15 by spoliart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@ int	main( void )
 	{
 		Character	deepCopy = Character("Eude");
 
-		deepCopy.equip(new Ice());
+		AMateria	*ice = new Ice();
+
+		deepCopy.equip(ice);
 		deepCopy.equip(new Cure());
 
 		Character	deepCopy2 = deepCopy;
@@ -28,22 +30,24 @@ int	main( void )
 
 		deepCopy.unequip(0);
 		deepCopy2.use(0, deepCopy);
+
+		delete ice;
 	}
 	std::cout << std::endl << std::endl;
 	{
-		IMateriaSource* src = new MateriaSource();
+		IMateriaSource*	src = new MateriaSource();
 		src->learnMateria(new Ice());
 		src->learnMateria(new Cure());
 
-		ICharacter* me = new Character("me");
+		ICharacter*	me = new Character("me");
 
-		AMateria* tmp;
+		AMateria*	tmp;
 		tmp = src->createMateria("ice");
 		me->equip(tmp);
 		tmp = src->createMateria("cure");
 		me->equip(tmp);
 
-		ICharacter* bob = new Character("bob");
+		ICharacter*	bob = new Character("bob");
 
 		me->use(0, *bob);
 		me->use(1, *bob);
